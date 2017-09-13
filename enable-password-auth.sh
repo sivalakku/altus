9976e83ce5c6c8e0957a54e00481fbe45bcc8076
Raghu K <raghuk.vit@gmail.com>
2017-09-13 02:08:31 +0000

    Updating altus

--- enable-password-auth.sh
+++ enable-password-auth.sh
@@ -2,14 +2,15 @@
 
 ### Note: I dont recomend this script in real times, It is only for Lab purpose.
 sed -i -e '/#PermitRootLogin/ c PermitRootLogin yes' -e '/^PasswordAuthentication/ c PasswordAuthentication yes' /etc/ssh/sshd_config
-echo -e "\e[31mEnter ROOT password: \e[0m"
+echo 'echo -n -e "\e[31mEnter ROOT password: \e[0m"
 read pass
 noc=$(echo $pass |wc -c)
-if [ $noc -lt 9 ]; then 
-	echo -e "\e[34m Root Password should be Minimum 8 characters\e[0m"
-	echo "Try Again"
-	exit 1
+if [ $noc -lt 9 ]; then
+        echo -e "\e[34m Root Password should be Minimum 8 characters\e[0m"
+        echo "Try Again"
+        exit 1
 fi
+echo $pass | passwd --stdin root &>/dev/null' >/tmp/set_root_pass 
+sh /tmp/set_root_pass
 
-echo $pass | passwd --stdin root &>/dev/null
 systemctl restart sshd
